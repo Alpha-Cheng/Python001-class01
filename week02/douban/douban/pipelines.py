983ee52f6033efcd68f9475cf12939a5cf589d78
chengzening <chengzening@qq.com>
2020-07-05 13:43:25 +0800

    '7.5.13'

--- week02/douban/douban/pipelines.py
+++ week02/douban/douban/pipelines.py
@@ -28,9 +28,9 @@ class DoubanPipeline:
                 PRIMARY KEY ( `id` )
                 )ENGINE=InnoDB DEFAULT CHARSET=utf8;'''
         
-        #sql1="INSERT INTO douban(`id`,`title`,`mold`,`release_time`) VALUES( 1,'{}','{}','{}')".format(item['title'],item['mold'],item['release_time']);
-        sql1="INSERT INTO movies(?,?,?) VALUES ('{title}', '{mold}', '{release_time}')".format(title=item['title'],mold=item['mold'],release_time=item['release_time']);
-    
+        #sql1="INSERT INTO douban(`title`,`mold`,`release_time`) VALUES('{title}', '{mold}', '{release_time}')",(item['title'],item['mold'],item['release_time']);
+        sql1="INSERT INTO douban(`title`,`mold`,`release_time`) VALUES ('{title}', '{mold}', '{release_time}')".format(title=item['title'], mold=' '.join(item['mold']), release_time=' '.join(item['release_time']));
+
         try:
             # 获得cursor游标对象
             con1 = conn.cursor()
@@ -39,8 +39,9 @@ class DoubanPipeline:
             print('-------------------------------------------')
             con1.execute(sql1)
             print('--------------------------------------------')
-            con1.commit()
+            conn.commit()
         except Exception as e:
+            print(sql1)
             print(e,'操作失败====================================')
         con1.close()
         conn.close()
