1470e8f93e8fdececd06d8b71449d073c124962b
chengzening <chengzening@qq.com>
2020-07-05 23:40:19 +0800

    '7.5.23'

--- week02/assignment02.py
+++ week02/assignment02.py
@@ -1,37 +1,35 @@
-from selenium import webdriver
 import time
+from selenium import webdriver
 
-class Login: 
-    def __init__(self):
-        self.login_url = "https://shimo.im/login?from=home"
-        # 需要填写个人账号/密码
-        self.mobile = '15200000000'
-        self.password = 'xxx123456'
+# 会话对象：在同一个 Session 实例发出的所有请求之间保持 cookie， 
+# 期间使用 urllib3 的 connection pooling 功能。
+# 向同一主机发送多个请求，底层的 TCP 连接将会被重用，从而带来显著的性能提升。
+try:
+    # 需要安装chrome driver, 和浏览器版本保持一致
+    # http://chromedriver.storage.googleapis.com/index.html
+    browser = webdriver.Chrome(executable_path='D:\Files\chromedriver_win32(1)39\chromedriver')
 
-    def run(self):
-        try:
-            browser = webdriver.Chrome()
-            browser.get(self.login_url)
-            time.sleep(1)
-            browser.find_element_by_xpath('//input[@name=\'mobileOrEmail\']').send_keys(self.mobile)
-            time.sleep(1)
-            browser.find_element_by_xpath('//input[@name=\'password\']').send_keys(self.password)
-            time.sleep(2)
-            browser.find_element_by_xpath("//button[contains(text(), '立即登录')]").click()
-            time.sleep(1)
+    browser.get('https://shimo.im/login?from=home')
+    time.sleep(1)
 
-            cookies = browser.get_cookies()
-            print(cookies)
-            time.sleep(3)
-        except Exception as e:
-           print(e)    
-        finally:
-            browser.close()
+    # browser.switch_to_frame(browser.find_elements_by_tag_name('iframe')[0])
+    # btm1 = browser.find_element_by_xpath('/html/body/div[1]/div[1]/ul[1]/li[2]')
+    # btm1.click()
 
-def main():
-    im = Login()
-    im.run()
+    browser.find_element_by_xpath('//input[@name="mobileOrEmail"]').send_keys('15146893256')
+    browser.find_element_by_xpath('//input[@name="password"]').send_keys('123456')
+    time.sleep(1)
+    browser.find_element_by_xpath('//button[contains(@class,"sm-button")]').click()
 
+    cookies = browser.get_cookies() # 获取cookies
+    #print(browser.page_source) #爬取的网页的源代码
+    print(cookies)
 
-if __name__ == '__main__':
-    main()
+    time.sleep(3)
+
+except Exception as e:
+    print('================================')
+    print(e)
+finally:    
+    browser.close()
+    
